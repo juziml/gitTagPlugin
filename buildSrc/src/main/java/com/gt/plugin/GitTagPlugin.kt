f73d782fb130cc25e38f0d67baf118f9c5df5524
rango <sink50@foxmail.com>
2021-02-17 18:41:39 +0800

    -

--- buildSrc/src/main/java/com/gt/plugin/GitTagPlugin.kt
+++ buildSrc/src/main/java/com/gt/plugin/GitTagPlugin.kt
@@ -2,13 +2,12 @@ package com.gt.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 
 class GitTagPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         //创建拓展函数，拓展函数名称，拓展函数类
         val extensionFun = project
-            .extensions.create("grockName", GitTagExtension::class.java)
+            .extensions.create("gitTagExtension", GitTagExtension::class.java)
         project.afterEvaluate {
             project.tasks.forEach {
                 val name = it.name
