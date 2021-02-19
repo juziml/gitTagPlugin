4797d78879d32ad0760685d5016ea4c35890ae5c
rango <sink50@foxmail.com>
2021-02-19 17:40:00 +0800

    -闭包测试

--- buildSrc/src/main/java/com/gt/plugin/GitTagExtension.kt
+++ buildSrc/src/main/java/com/gt/plugin/GitTagExtension.kt
@@ -1,5 +1,9 @@
 package com.gt.plugin
 
+import groovy.lang.Closure
+import org.gradle.util.ConfigureUtil
+import javax.swing.Action
+
 /**
  *@Desc:
  *-
@@ -9,6 +13,37 @@ package com.gt.plugin
 //作为拓展函数对象，不能是final
 open class GitTagExtension {
     var targetTask = "taskName"
-    var tagName = "tagName"
-    var tagMsg = "-"
+    //准备一个接受闭包的方法
+    fun  cTest(cc:Closure<Any>){
+        println("call cTest")
+        val tagConfig = TagConfig()
+        cc.resolveStrategy = Closure.DELEGATE_ONLY
+        cc.delegate = tagConfig
+        cc.call()
+        println("call cTest tagConfig.name =${tagConfig.mName}")
+    }
+    var tag:Closure<Any>? = null
+
+}
+open class TagHandler(private val closure:Closure<in Any>){
+    fun call():TagConfig{
+        val tagConfig = TagConfig()
+        closure.resolveStrategy = Closure.DELEGATE_ONLY
+        closure.delegate = tagConfig
+        closure.call()
+//        ConfigureUtil.configure(closure,tagConfig)
+        return tagConfig
+    }
+}
+
+open class TagConfig{
+    var mName:String = ""
+    var mMsg:String = ""
+
+    fun name(s:String){
+        mName = s
+    }
+    fun msg(s:String){
+        mMsg = s
+    }
 }
--- buildSrc/src/main/java/com/gt/plugin/GitTagPlugin.kt
+++ buildSrc/src/main/java/com/gt/plugin/GitTagPlugin.kt
@@ -1,35 +1,18 @@
 package com.gt.plugin
-
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class GitTagPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         //创建拓展函数，拓展函数名称，拓展函数类
-        val extensionFun = project
+        val exFun = project
             .extensions.create("autoPublishTag", GitTagExtension::class.java)
         project.afterEvaluate {
-            project.tasks.forEach {
-                val name = it.name
-                if (name == extensionFun.targetTask) {
-                    it.doLast {
-                        val  createTag = "git tag ${extensionFun.tagName} -m ${extensionFun.tagMsg}"
-                        println("createTag:$createTag")
-                        Runtime.getRuntime().exec(createTag)
-                    }
-                    it.doLast {
-                        println("createTag:sleep"+System.currentTimeMillis())
-                        Thread.sleep(2000)
-                        println("createTag:sleep"+System.currentTimeMillis())
-                    }
-                    //需要先后执行的命令，不能放在一个节点里，命令执行似乎是异步并发的
-                    it.doLast {
-                        val pushTag = "git push origin ${extensionFun.tagName}"
-                        println("pushTag:$pushTag")
-                        Runtime.getRuntime().exec(pushTag)
-                    }
-                }
-            }
+
+            println("extensionFun taskName = ${exFun.targetTask} ")
+            val tagHandler = TagHandler(exFun.tag!!)
+           val tagConfig =  tagHandler.call()
+            println("extensionFun taskName = ${tagConfig.mName} ")
         }
     }
 }
