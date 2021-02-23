9f68f5974a5bd3fc34286bdf3f26aeb5261856a5
rango <sink50@foxmail.com>
2021-02-23 21:37:50 +0800

    -

---
+++ depTestLib/src/main/java/com/grock/deptestlib/LibTest.kt
@@ -0,0 +1,13 @@
+package com.grock.deptestlib
+
+/**
+ *@Desc:
+ *-
+ *-
+ *create by zhusw on 2021/2/23 19:41
+ */
+object LibTest {
+    fun test(){
+        println("-----------LibTest-----------------")
+    }
+}
